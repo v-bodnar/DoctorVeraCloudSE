3549161b808dbeaf8e73b82e11baefb6300242e5
bodik@list.ru <bodik@list.ru>
2014-10-14 15:02:09 +0300

    Commit from JBoss Tools

--- src/main/java/jdbc/PreparedStatementServlet.java
+++ src/main/java/jdbc/PreparedStatementServlet.java
@@ -14,8 +14,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.sql.*;
 
-@WebServlet(name = "uploads",urlPatterns = {"/uploads/*"})
-@MultipartConfig
 public class PreparedStatementServlet extends HttpServlet {
 	  public void init() throws ServletException
 	  {
