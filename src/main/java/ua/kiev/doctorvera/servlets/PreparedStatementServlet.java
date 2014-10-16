7ddd47e648ccc5f0da5361163d8d334acf6fabe3
bodik@list.ru <bodik@list.ru>
2014-10-16 14:41:24 +0300

    Commit from JBoss Tools

--- src/main/java/ua/kiev/doctorvera/servlets/PreparedStatementServlet.java
+++ src/main/java/ua/kiev/doctorvera/servlets/PreparedStatementServlet.java
@@ -120,6 +120,7 @@ public class PreparedStatementServlet extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
         response.setContentType("text/html");
+        request.setCharacterEncoding("UTF-8");
         PrintWriter out = response.getWriter();
         
         try {
