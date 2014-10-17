f512c8d10dbcc6aff12f8636a12dacf4fd4c4305
bodik@list.ru <bodik@list.ru>
2014-10-18 00:02:27 +0300

    Commit from JBoss Tools

--- src/main/java/ua/kiev/doctorvera/servlets/Test.java
+++ src/main/java/ua/kiev/doctorvera/servlets/Test.java
@@ -35,6 +35,7 @@ public class Test extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
         response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
         request.setCharacterEncoding("UTF-8");
         PrintWriter out = response.getWriter();
         
