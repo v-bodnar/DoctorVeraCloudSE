fb723ef0d73e1b5b2477dacb7198146e16dcfb58
bodik@list.ru <bodik@list.ru>
2014-10-24 14:46:03 +0300

    xvbxb

---
+++ src/main/java/ua/kiev/doctorvera/servlets/Test.java
@@ -0,0 +1,15 @@
+package ua.kiev.doctorvera.servlets;
+
+import java.util.ArrayList;
+
+import ua.kiev.doctorvera.entity.UserTypes;
+import ua.kiev.doctorvera.mysql.MySqlDaoFactory;
+import ua.kiev.doctorvera.mysql.UserTypesMySql;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+	}
+
+}
