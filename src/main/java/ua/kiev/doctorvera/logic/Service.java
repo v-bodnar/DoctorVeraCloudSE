be0b1e3f1140648a7151090f3b1369e4ce82e9b2
bodik@list.ru <bodik@list.ru>
2014-10-31 18:53:58 +0200

    usertag creating

---
+++ src/main/java/ua/kiev/doctorvera/logic/Service.java
@@ -0,0 +1,29 @@
+package ua.kiev.doctorvera.logic;
+
+import java.security.NoSuchAlgorithmException;
+
+public class Service {
+	
+	  public static String encrypt(String x){
+		    java.security.MessageDigest d = null;
+		    try {
+				d = java.security.MessageDigest.getInstance("SHA-1");
+			} catch (NoSuchAlgorithmException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		    d.reset();
+		    d.update(x.getBytes());
+		    
+		    return byteArrayToHexString(d.digest());
+		  }
+	  
+	  private static String byteArrayToHexString(byte[] b) {
+		  String result = "";
+		  for (int i=0; i < b.length; i++) {
+		    result +=
+		          Integer.toString( ( b[i] & 0xff ) + 0x100, 16).substring( 1 );
+		  }
+		  return result;
+		}
+}
---
+++ src/main/java/ua/kiev/doctorvera/usertag/Menu.java
@@ -0,0 +1,74 @@
+package ua.kiev.doctorvera.usertag;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map.Entry;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.TagSupport;
+
+import ua.kiev.doctorvera.manager.Mapping;
+import ua.kiev.doctorvera.manager.Message;
+@SuppressWarnings("rawtypes")
+public class Menu extends TagSupport {
+
+	private static final long serialVersionUID = 1L;
+	
+	private final Message messages = Message.getInstance();
+	private final Mapping mapper = Mapping.getInstance();
+	private final HashMap<Enum, Enum> adminItems = new HashMap<Enum, Enum>();
+	private final HashMap<Enum, Enum> managerItems = new HashMap<Enum, Enum>();
+	private final HashMap<Enum, Enum> financeItems = new HashMap<Enum, Enum>();
+	private final HashMap<Enum, Enum> statItems = new HashMap<Enum, Enum>();
+	
+	public int doStartTag() throws JspException {
+		adminItems.put(Mapping.Key.USERS_PAGE, Message.Menu.MENU_ITEM_USERS);
+		adminItems.put(Mapping.Key.ADD_USER_PAGE, Message.Menu.MENU_ITEM_ADD_USER);
+		adminItems.put(Mapping.Key.USER_GROUPS_PAGE, Message.Menu.MENU_ITEM_USER_GROUPS);
+		try { 
+			pageContext.getOut().write("<h3>" + messages.getMessage(Message.Menu.MENU_HEADER) + "</h3>");
+			
+			pageContext.getOut().write(messages.getMessage(Message.Menu.ADMIN_BLOCK_HEADER));
+			pageContext.getOut().write("<ul>");
+			for(Entry<Enum, Enum> menuItem : adminItems.entrySet()) {
+				pageContext.getOut().write("<li>");
+				pageContext.getOut().write("<a href = " + mapper.getProperty(menuItem.getKey()) + ">" + messages.getMessage(menuItem.getValue()) + "</a>");
+				pageContext.getOut().write("</li>");
+			}
+			pageContext.getOut().write("</ul>");
+			
+			pageContext.getOut().write(messages.getMessage(Message.Menu.MANAGER_BLOCK_HEADER));
+			pageContext.getOut().write("<ul>");
+			for(Entry<Enum, Enum> menuItem : managerItems.entrySet()) {
+				pageContext.getOut().write("<li>");
+				pageContext.getOut().write("<a href = " + mapper.getProperty(menuItem.getKey()) + ">" + messages.getMessage(menuItem.getValue()) + "</a>");
+				pageContext.getOut().write("</li>");
+			}
+			pageContext.getOut().write("</ul>");
+			
+			pageContext.getOut().write(messages.getMessage(Message.Menu.FINANCIAL_BLOCK_HEADER));
+			pageContext.getOut().write("<ul>");
+			for(Entry<Enum, Enum> menuItem : financeItems.entrySet()) {
+				pageContext.getOut().write("<li>");
+				pageContext.getOut().write("<a href = " + mapper.getProperty(menuItem.getKey()) + ">" + messages.getMessage(menuItem.getValue()) + "</a>");
+				pageContext.getOut().write("</li>");
+			}
+			pageContext.getOut().write("</ul>");
+			
+			pageContext.getOut().write(messages.getMessage(Message.Menu.STATISTICS_BLOCK_HEADER));
+			pageContext.getOut().write("<ul>");
+			for(Entry<Enum, Enum> menuItem : statItems.entrySet()) {
+				pageContext.getOut().write("<li>");
+				pageContext.getOut().write("<a href = " + mapper.getProperty(menuItem.getKey()) + ">" + messages.getMessage(menuItem.getValue()) + "</a>");
+				pageContext.getOut().write("</li>");
+			}
+			pageContext.getOut().write("</ul>");
+			
+			
+			
+		} catch (IOException e) { 
+			e.printStackTrace(); 
+		} 
+		return SKIP_BODY;
+	}
+}
