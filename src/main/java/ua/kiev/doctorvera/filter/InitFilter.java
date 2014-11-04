dccdd6126d5553ece4ced121ae87d928521b6ccd
oper4 <oper4@192.168.28.198>
2014-11-04 20:35:07 +0200

    asfsf

---
+++ src/main/java/ua/kiev/doctorvera/filter/InitFilter.java
@@ -0,0 +1,34 @@
+package ua.kiev.doctorvera.filter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import ua.kiev.doctorvera.manager.Message;
+
+// Implements Filter class
+public class InitFilter implements Filter {
+	public void init(FilterConfig config) throws ServletException {
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws java.io.IOException, ServletException {
+
+		//Message message = Message.getInstance();
+		//String welcome = message.getMessage(Message.Messages.HEADER_WELCOME);
+		//System.out.println("Class Message initialized! " + welcome);
+
+		// Pass request back down the filter chain
+		chain.doFilter(request, response);
+	}
+
+	public void destroy() {
+		/*
+		 * Called before the Filter instance is removed from service by the web
+		 * container
+		 */
+	}
+}
